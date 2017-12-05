daa875367458a122a8a1736b372ccd3674c39487
jovanka <jovanka.placeres@gmail.com>
2017-12-05 05:12:22 +0000

    routes, templates, template helpers, mvc, middleware, errors

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const storeController = require('../controllers/storeController');
 
 // Do work here
-router.get('/', (req, res) => {
-	// console.log('testing');
-	const jo = { name: 'jo', age: 50, awesome: true };
-  // res.send('Hey! It works!');
-  // res.json(jo);
-  // res.send(req.query.name);
-  // res.json(req.query);
-  res.render('hello', {
-  	name: 'jo',
-  	dog: 'enigma',
-  	title: 'nomz'
-  });
-});
+router.get('/', storeController.myMiddleware, storeController.homePage);
 
-router.get('/reverse/:name', (req, res) => {
-	const reverse = [...req.params.name].reverse().join('');
-	res.send(reverse);
-});
 
 module.exports = router;
