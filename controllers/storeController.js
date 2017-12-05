9597fb546759812e8ba0d3810909abf65374a35d
jovanka <jovanka.placeres@gmail.com>
2017-12-05 23:24:42 +0000

    database storage-- store model, saving and mixins

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -1,9 +1,18 @@
-exports.myMiddleware = (req, res, next) => {
-	req.name = "jo";
-	next();
-}
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
 	console.log(req.name);
 	res.render('index');
-}
+};
+
+exports.addStore = (req, res) => {
+	// res.send('this works'); // testing
+	res.render('editStore', {title: 'add store'});
+};
+
+exports.createStore = (req, res) => {
+	// console.log(req.body);
+	// res.json(req.body);
+	const store = new Store(req.body);
+};
---
+++ models/Store.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const slug = require('slugs');
+
+const storeSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		trim: true,
+		required: 'Oops! Seems you forgot a store name, please enter one.'
+	},
+	slug: String,
+	description: {
+		type: String,
+		trim: true
+	},
+	tags: [String]
+});
+
+// will be coming back to this-- presently 2 stores w/ same name would have overlap slugs (no bueno)
+storeSchema.pre('save', function(next) {
+	if(!this.isModified('name')) {
+		next(); // skips
+		return; // stops function
+	}
+	this.slug = slug(this.name);
+	next();
+});
+
+module.exports = mongoose.model('Store', storeSchema);
