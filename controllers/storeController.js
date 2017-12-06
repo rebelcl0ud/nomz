80d5b91eb2d22d04abc116e610cbd0afe87c2dc1
jovanka <jovanka.placeres@gmail.com>
2017-12-06 03:16:21 +0000

    flash messages

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
-	console.log(req.name);
 	res.render('index');
 };
 
@@ -11,8 +10,16 @@ exports.addStore = (req, res) => {
 	res.render('editStore', {title: 'add store'});
 };
 
-exports.createStore = (req, res) => {
+exports.createStore = async (req, res) => {
 	// console.log(req.body);
 	// res.json(req.body);
-	const store = new Store(req.body);
+
+	// es6 promises
+	const store = await(new Store(req.body)).save();
+	req.flash('success', `${store.name} has been successfully created`);
+	res.redirect(`/store/${store.slug}`);
 };
+
+
+
+
