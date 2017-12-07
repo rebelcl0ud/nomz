862f66aa5b86c82f80bff62566d7a9dc17883af6
jovanka <jovanka.placeres@gmail.com>
2017-12-07 04:17:57 +0000

    query db and create edit/update functionality

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -7,7 +7,7 @@ exports.homePage = (req, res) => {
 
 exports.addStore = (req, res) => {
 	// res.send('this works'); // testing
-	res.render('editStore', {title: 'add store'});
+	res.render('editStore', { title: 'add store' });
 };
 
 exports.createStore = async (req, res) => {
@@ -16,10 +16,35 @@ exports.createStore = async (req, res) => {
 
 	// es6 promises
 	const store = await(new Store(req.body)).save();
-	req.flash('success', `${store.name} has been successfully created`);
+	req.flash('success', `${store.name} has been successfully created. Leave a review?`);
 	res.redirect(`/store/${store.slug}`);
 };
 
+exports.getStores = async (req, res) => {
+	// 1. query db for list of all stores
+	const stores = await Store.find();
+	// console.log(stores);
+	res.render('stores', { title: 'Stores', stores: stores });
+};
 
+exports.editStore = async (req, res) => {
+	// 1. find store given id
+	// res.json(req.params);
+	const store = await Store.findOne({ _id: req.params.id });
+	// 2. confirm user, owner of store TODO
+	// 3. render edit form so user can update store
+	res.render('editStore', { title: `Edit ${store.name}`, store: store })
+}
+
+exports.updateStore = async (req, res) => {
+	// 1. find/update store
+	// redirect store and show it worked
+	const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body, {
+		new: true, // return new store instead of old one
+		runValidators: true
+	}).exec();
+	req.flash('success', `Successfully updated ${store.name} <a href="/stores/${store.slug}"> View Store </a>`);
+	res.redirect(`/stores/${store.id}/edit`);
+}
 
 
